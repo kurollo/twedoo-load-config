7159af0068cffc82cfc9d64403ad2c7846988a70
Houssem Maamria <maamria.houssem@gmail.com>
2018-09-11 16:00:58 +0100

    load routes from each bundle inside directories

--- RoutingInjection/RouteServiceProvider.php
+++ RoutingInjection/RouteServiceProvider.php
@@ -12,24 +12,49 @@ namespace symfony\Twedoo\RoutingInjection;
 
 use Symfony\Component\Config\Loader\Loader;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Yaml\Yaml;
 
 class RouteServiceProvider extends Loader
 {
 
+    public $getDirectories = [];
+    public $ignoreDir = ['..', '.'];
+    public $getBundles = [];
+
     public function load($resource, $type = null)
     {
-        $dir = getcwd().'/src/';
 
-        $getBundles = preg_grep('/^([^.])/', array_diff(scandir($dir, 1), array('..', '.')));
-        $routes = new RouteCollection();
-        foreach ($getBundles as $bundle)
+        $dir = getcwd().'/app/config';
+        $this->getDirectories = Yaml::parse(file_get_contents($dir.'/config.yml'))['parameters']['directories_inside_src'];
+
+        if(is_array($this->getDirectories))
         {
-            if(file_exists($dir.$bundle.'/Resources/config/routing.yml'))
+            foreach ($this->getDirectories as $key => $dirName)
             {
-                $resource = '@'.$bundle.'/Resources/config/routing.yml';
-                $type = 'yaml';
-                $importedRoutes = $this->import($resource, $type);
-                $routes->addCollection($importedRoutes);
+                $dir = getcwd().'/src/'.$dirName.'/';
+                if (is_dir($dir )) {
+                    $this->getBundles[$dir] = preg_grep('/^([^.])/', array_diff(scandir($dir, 1), $this->ignoreDir));
+                }
+            }
+            $this->ignoreDir = array_merge($this->ignoreDir, $this->getDirectories);
+        }
+
+        $dir = getcwd().'/src/';
+        $this->getBundles[$dir] = preg_grep('/^([^.])/', array_diff(scandir($dir, 1),  $this->ignoreDir));
+
+        $routes = new RouteCollection();
+
+        foreach ($this->getBundles as $path => $bundles)
+        {
+            foreach ($bundles as $bundle)
+            {
+                if(file_exists($path.$bundle.'/Resources/config/routing.yml'))
+                {
+                    $resource = '@'.$bundle.'/Resources/config/routing.yml';
+                    $type = 'yaml';
+                    $importedRoutes = $this->import($resource, $type);
+                    $routes->addCollection($importedRoutes);
+                }
             }
         }
         return  $routes;
